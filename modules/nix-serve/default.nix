e1b9e5c4b25fe4ca223758c0aa9a8f5ad2dc134b
Fernando Ayats <ayatsfer@gmail.com>
2022-04-30 16:01:21 +0200

    nginx theme

--- modules/nix-serve/default.nix
+++ modules/nix-serve/default.nix
@@ -1,9 +1,30 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   inherit (builtins) toString;
   cache-port = 5000;
   # http-store-port = 5444;
   cache-ip = "192.168.100.33";
   http-store-ip = "192.168.100.22";
+
+  nginx-theme = with pkgs;
+    stdenv.mkDerivation {
+      pname = "nginx-fancyindex-flat-theme";
+      version = "1.1";
+
+      dontBuild = true;
+
+      src = fetchzip {
+        url = "https://github.com/alehaa/nginx-fancyindex-flat-theme/releases/download/v1.1/nginx-fancyindex-flat-theme-1.1.tar.gz";
+        sha256 = "sha256-eT3D9hPlts3S+bzsGYz8KCNmaDcrPiiCRzVJLq8PDlA=";
+      };
+
+      installPhase = ''
+        cp -r $src $out
+      '';
+    };
 in {
   # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html
   containers.nix-serve = {
@@ -43,11 +64,22 @@ in {
       networking.firewall.allowedTCPPorts = [80];
       services.nginx = {
         enable = true;
-        virtualHosts."localhost".locations."~ /(.+)-(.+)" = {
-          root = "/nix/store";
-          extraConfig = ''
-            autoindex on;
-          '';
+        additionalModules = [pkgs.nginxModules.fancyindex];
+        virtualHosts."localhost".locations = {
+          "~ /(.+)-(.+)" = {
+            root = "/nix/store";
+            extraConfig = ''
+              fancyindex on;
+              fancyindex_exact_size off;
+              fancyindex_header "${lib.removePrefix "/nix/store" nginx-theme.outPath}/header.html";
+              fancyindex_footer "${lib.removePrefix "/nix/store" nginx-theme.outPath}/footer.html";
+              fancyindex_show_path off;
+              fancyindex_name_length 255;
+              fancyindex_time_format "-";
+              fancyindex_default_sort name;
+              fancyindex_directories_first on;
+            '';
+          };
         };
       };
     };
