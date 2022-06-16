105387965a262bd1ab5e4e6fb8b5eb8e7539a7b3
Fernando Ayats <ayatsfer@gmail.com>
2022-06-16 15:58:01 +0200

    lagos: fill with placeholder

--- flake.nix
+++ flake.nix
@@ -81,14 +81,26 @@
       // (mkSystems {
         system = "x86_64-linux";
         basename = "lagos";
-        variants = rec {
-          vm = [
+        variants = let
+          base = [
             "${modulesPath}/profiles/minimal.nix"
             ./modules/lagos/common.nix
             ./modules/lagos/step.nix
           ];
+        in {
+          vm =
+            base
+            ++ [
+              {
+                fileSystems."/" = {
+                  fsType = "tmpfs";
+                  device = "none";
+                };
+                boot.loader.grub.device = "/dev/null";
+              }
+            ];
           prod =
-            vm
+            base
             ++ [
               "${modulesPath}/virtualisation/google-compute-image.nix"
               {
