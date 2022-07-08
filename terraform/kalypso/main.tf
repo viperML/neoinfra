110c8fa8f3d525802405610739a8509f4b5b44ae
Fernando Ayats <ayatsfer@gmail.com>
2022-07-08 20:47:50 +0200

    kalypso: oracle networking and auth

--- terraform/kalypso/main.tf
+++ terraform/kalypso/main.tf
@@ -24,23 +24,57 @@ data "oci_core_image" "kalypso" {
 }
 
 
-
-
-
-resource "oci_core_vcn" "terraform_vnc" {
+resource "oci_core_vcn" "terraform_vcn" {
   compartment_id = var.compartment_id
-  display_name   = "terraform_vnc"
+  display_name   = "terraform0"
   cidr_blocks = [
     "10.0.0.0/16"
   ]
-  # TODO need to setup DNS ?
+  dns_label = "terraform0"
+}
+
+resource "oci_core_route_table" "terraform_vcn_route0" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.terraform_vcn.id
+  display_name   = "Internet Gateway"
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.terraform_vcn_gateway.id
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_internet_gateway" "terraform_vcn_gateway" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.terraform_vcn.id
+  enabled        = true
+  display_name   = "terraform gateway"
+}
+
+resource "oci_core_security_list" "terraform_subnet_security_list" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.terraform_vcn.id
+  display_name   = "Terraform Security Lists"
+  ingress_security_rules {
+    protocol  = "all"
+    source    = "0.0.0.0/0"
+    stateless = true
+  }
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
 }
 
 resource "oci_core_subnet" "terraform_subnet" {
-    cidr_block = "10.0.0.0/24"
-    compartment_id = var.compartment_id
-    vcn_id = oci_core_vcn.terraform_vnc.id
-    display_name = "terraform_subnet"
+  cidr_block     = "10.0.0.0/24"
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.terraform_vcn.id
+  display_name   = "terraform_subnet"
+  security_list_ids = [
+    oci_core_security_list.terraform_subnet_security_list.id
+  ]
+  route_table_id = oci_core_route_table.terraform_vcn_route0.id
 }
 
 
@@ -58,8 +92,35 @@ resource "oci_core_instance" "kalypso" {
     source_id   = data.oci_core_image.kalypso.image_id
   }
   create_vnic_details {
-    assign_public_ip = "false"
-    display_name = "kalypso_vnic"
-    subnet_id = oci_core_subnet.terraform_subnet.id
+    assign_public_ip          = true
+    display_name              = "kalypso_vnic"
+    subnet_id                 = oci_core_subnet.terraform_subnet.id
+    assign_private_dns_record = false
   }
+}
+
+variable "oci_key_id" {
+  type        = string
+  description = "Vault Key OCID"
+}
+
+resource "oci_identity_policy" "vault_policy" {
+  compartment_id = var.compartment_id
+  description    = "Policies for kalypso to access Vault"
+  name           = "TerraformVault"
+  statements = [
+    "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to {AUTHENTICATION_INSPECT} in tenancy",
+    "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to {GROUP_MEMBERSHIP_INSPECT} in tenancy",
+    "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to use keys in compartment id ${var.compartment_id} where target.key.id = '${var.oci_key_id}'",
+    "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to manage buckets in compartment id ${var.compartment_id}",
+    "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to manage objects in compartment id ${var.compartment_id}",
+    "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to use secrets in compartment id ${var.compartment_id}"
+  ]
+}
+
+resource "oci_identity_dynamic_group" "vault_dynamic_group" {
+    compartment_id = var.compartment_id
+    name = "TerraformVault"
+    description = "Group holding instances that should access Vault"
+    matching_rule = "instance.id = '${oci_core_instance.kalypso.id}'"
 }
