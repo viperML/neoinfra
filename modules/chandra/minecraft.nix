3996926c2fcfb0d9d26ed0eb568df7a3d3249f24
Fernando Ayats <ayatsfer@gmail.com>
2022-09-15 19:30:05 +0200

    parametric server name

--- modules/chandra/minecraft.nix
+++ modules/chandra/minecraft.nix
@@ -1,14 +1,15 @@
 {...}: let
   minecraftPort = 25565;
+  name = "wuselcraft-create-edition";
 in {
-  virtualisation.oci-containers.containers."minecraft-aof" = {
+  virtualisation.oci-containers.containers."minecraft-${name}" = {
     image = "ghcr.io/viperml/minecraft-serverstarter-docker:v2.0.0";
     ports = [
       "${toString minecraftPort}:${toString minecraftPort}"
     ];
     volumes = [
-      "wuselcraft01:/srv/minecraft"
-      "${./wuselcraft-create-edition.yaml}:/srv/minecraft-vendor/server-setup-config.yaml"
+      "${name}:/srv/minecraft"
+      "${./. + "/${name}.yaml"}:/srv/minecraft-vendor/server-setup-config.yaml"
     ];
   };
 
