e82eecebcadfb5fde63e3fb135acd198d8d13135
Fernando Ayats <ayatsfer@gmail.com>
2022-09-15 18:09:40 +0200

    use generic minecraft serverstarter

--- modules/chandra/minecraft.nix
+++ modules/chandra/minecraft.nix
@@ -2,12 +2,13 @@
   minecraftPort = 25565;
 in {
   virtualisation.oci-containers.containers."minecraft-aof" = {
-    image = "ghcr.io/viperml/aof-docker:v1.0.5";
+    image = "ghcr.io/viperml/minecraft-serverstarter-docker:v2.0.0";
     ports = [
       "${toString minecraftPort}:${toString minecraftPort}"
     ];
     volumes = [
-      "aof_runtime:/srv/aof-runtime"
+      "wuselcraft01:/srv/minecraft"
+      "${./wuselcraft-create-edition.yaml}:/srv/minecraft-vendor/server-setup-config.yaml"
     ];
   };
 
