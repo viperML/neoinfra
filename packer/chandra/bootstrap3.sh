c2447e22be51fc79290694c82813ef81df6ca5b9
Fernando Ayats <ayatsfer@gmail.com>
2022-09-24 21:27:53 +0200

    no chandra packer

--- packer/chandra/bootstrap3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-mkdir -p /new-var/lib/secrets
-mv -vf /home/ubuntu/chandra.age /new-var/lib/secrets
-chown -R root:root /new-var/lib/secrets
-chmod 600 /new-var/lib/secrets/*.age
-
-rm -rf /etc
