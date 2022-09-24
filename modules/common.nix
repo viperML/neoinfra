21fcb6915c2babe4fcbb751f0f459f87cc263ff4
Fernando Ayats <ayatsfer@gmail.com>
2022-09-24 21:24:50 +0200

    auto gc

--- modules/common.nix
+++ modules/common.nix
@@ -1,21 +1,33 @@
 {...}: {
   documentation.enable = false;
+
   environment.defaultPackages = [];
+
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
   ];
+
   time.timeZone = "UTC";
-  nix.settings = {
-    extra-experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    extra-substituters = [
-      "https://viperml.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "viperml.cachix.org-1:qZhKBMTfmcLL+OG6fj/hzsMEedgKvZVFRRAhq7j8Vh8="
-    ];
-  };
+
   users.mutableUsers = false;
+
+  nix = {
+    gc = {
+      automatic = true;
+      options = "-d 14d";
+    };
+
+    settings = {
+      extra-experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      extra-substituters = [
+        "https://viperml.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "viperml.cachix.org-1:qZhKBMTfmcLL+OG6fj/hzsMEedgKvZVFRRAhq7j8Vh8="
+      ];
+    };
+  };
 }
