e4a8adc82c536f3e59b637a65842022a1d46cb39
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-04-18 09:53:21 +0200

    tweak mautrix

--- modules/matrix-bridge-whatsapp/default.nix
+++ modules/matrix-bridge-whatsapp/default.nix
@@ -1,15 +1,24 @@
 {
   config,
   pkgs,
-  lib, ...
-}: {
-
-  services.mautrix-whatsapp = {};
-
+  lib,
+  ...
+}:
+{
   assertions = [
     {
       assertion = config.services.matrix-synapse.enable;
       message = "Mautrix whatsapp requires matrix";
     }
+  ];
+
+  imports = [ ./settings.nix ];
+
+  services.mautrix-whatsapp = {
+    enable = true;
+  };
+
+  services.matrix-synapse.settings.app_service_config_files = [
+    "/var/lib/mautrix-whatsapp/whatsapp-registration.yaml"
   ];
 }
