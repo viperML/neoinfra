f4579e0be9401cf40544c4191a8da7a6376ef5a9
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-05-22 10:42:26 +0200

    update noshell

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -10,7 +10,10 @@
   ];
   prefix = "/var/lib/tailscale/ssh";
 in {
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    extraUpFlags = ["--ssh"];
+  };
   networking.firewall.interfaces.${config.services.tailscale.interfaceName}.allowedTCPPorts = [22];
   networking.firewall.checkReversePath = "loose";
 
@@ -19,6 +22,7 @@ in {
     extraConfig = lib.mkOrder 0 ''
       ${lib.concatMapStringsSep "\n" (k: "HostKey ${prefix}/${k}") keyNames}
     '';
+    hostKeys = lib.mkForce [];
   };
 
   systemd.paths = lib.mapAttrs' (name: value: lib.nameValuePair "tailscale-${name}" value) (lib.genAttrs keyNames (key: {
@@ -34,5 +38,9 @@ in {
     script = ''
       systemctl try-restart sshd.service
     '';
+  };
+
+  systemd.services."sshd" = {
+    after = ["tailscaled.service"];
   };
 }
