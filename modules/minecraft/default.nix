68ff6f71b6a96acab9e4811799d0573b776cc2f4
Fernando Ayats <ayatsfer@gmail.com>
2024-09-07 09:49:40 +0200

    add easier sleeping

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -43,6 +43,8 @@ in {
       CURSEFORGE_FILES = lib.concatStringsSep "," [
         # TODO: sync with index.html
         "https://www.curseforge.com/minecraft/mc-mods/simple-voice-chat/files/5676800"
+        # server only
+        "https://www.curseforge.com/minecraft/mc-mods/easier-sleeping/files/4628693"
       ];
     };
     environmentFiles = [
