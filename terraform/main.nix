d7f3947fcb35b6208a743adbb367a429b3b27d1e
Fernando Ayats <fayats@bsc.es>
2025-09-19 15:39:09 +0200

    Cleanup terraform

--- terraform/main.nix
+++ terraform/main.nix
@@ -46,16 +46,6 @@ in
     region = "eu-marseille-1";
   };
 
-  # module."network" = {
-  #   source         = "./network"
-  #   compartment_id = var.compartment_id
-  # }
-
-  # module "images" {
-  #   source         = "./images"
-  #   compartment_id = var.compartment_id
-  # }
-
   variable."deploy" = {
     type = "bool";
     default = false;
@@ -145,101 +135,4 @@ in
       # })
     };
   };
-
-  # output."cloudinit_config_shiva_raw" = {
-  #   value = FIXME;
-  #   # value = templatefile("cloud-config.yaml.tftpl", {
-  #   #   ssh_public_key = jsonencode(data.local_file.ssh_public_key.content)
-  #   #   age_key        = jsonencode(data.local_file.shiva_age.content)
-  #   # })
-  #   sensitive = true;
-  # };
-
-  # module "aarch64-kexec-installer-noninteractive" {
-  #   source = "github.com/numtide/nixos-anywhere//terraform/nix-build"
-  #   attribute = ".#packages.aarch64-linux.kexec-installer-noninteractive"
-  # }
-
-  # module "shiva_deploy" {
-  #   source = "github.com/numtide/nixos-anywhere//terraform/all-in-one"
-  #   nixos_system_attr = ".#nixosConfigurations.aarch64-linux-oci-installer.config.system.build.toplevel"
-  #   nixos_partitioner_attr = ".#nixosConfigurations.aarch64-linux-oci-installer.config.system.build.diskoNoDeps"
-  #   kexec_tarball_url = "${module.aarch64-kexec-installer-noninteractive.result.out}/nixos-kexec-installer-noninteractive-aarch64-linux.tar.gz"
-  #   target_host = oci_core_instance.shiva.public_ip
-  #   install_user = "root"
-  #   instance_id = oci_core_instance.shiva.id
-  # }
-
-  # vishnu
-
-  /*
-    resource "oci_core_instance" "vishnu" {
-      display_name        = "terraform-visnhu"
-      availability_domain = "vOMn:EU-MARSEILLE-1-AD-1"
-      compartment_id      = var.compartment_id
-      shape               = "VM.Standard.E2.1.Micro"
-      create_vnic_details {
-        assign_public_ip          = true
-        subnet_id                 = module.network.terraform_subnet.id
-        assign_private_dns_record = false
-      }
-      source_details {
-        source_type = "image"
-        source_id   = module.images.always-free
-      }
-      lifecycle {
-        ignore_changes = [
-          source_details,
-          metadata
-        ]
-      }
-      metadata = {
-        user_data = data.cloudinit_config.visnhu.rendered
-      }
-    }
-
-    output "vishnu_ip" {
-      value = oci_core_instance.vishnu.public_ip
-    }
-
-    data "local_file" "vishnu_age" {
-      filename = "../secrets/vishnu.age"
-    }
-
-    data "cloudinit_config" "visnhu" {
-      gzip          = false
-      base64_encode = true
-      part {
-        filename     = "cloud-config.yaml"
-        content_type = "text/cloud-config"
-        content = templatefile("cloud-config.yaml.tftpl", {
-          ssh_public_key = jsonencode(data.local_file.ssh_public_key.content)
-          age_key        = jsonencode(data.local_file.vishnu_age.content)
-        })
-      }
-    }
-  */
-
-  # resource "oci_identity_dynamic_group" "vault_dynamic_group" {
-  #   compartment_id = var.compartment_id
-  #   name           = "TerraformVaultGroup"
-  #   description    = "Group holding instances that should access Vault"
-  #   matching_rule  = "instance.id = '${oci_core_instance.vishnu.id}'"
-  # }
-
-  # resource "oci_identity_policy" "vault_policy" {
-  #   compartment_id = var.compartment_id
-  #   description    = "Policies to access Vault"
-  #   name           = "TerraformVaultPolicy"
-  #   statements = [
-  #     # ocikms
-  #     "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to use keys in compartment id ${var.compartment_id} where target.key.id = '${var.oci_key_id}'",
-  #     # object storage
-  #     "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to {AUTHENTICATION_INSPECT} in tenancy",
-  #     "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to {GROUP_MEMBERSHIP_INSPECT} in tenancy",
-  #     "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to manage buckets in compartment id ${var.compartment_id}",
-  #     "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to manage objects in compartment id ${var.compartment_id}",
-  #     "allow dynamic-group ${oci_identity_dynamic_group.vault_dynamic_group.name} to use secrets in compartment id ${var.compartment_id}"
-  #   ]
-  # }
 }
